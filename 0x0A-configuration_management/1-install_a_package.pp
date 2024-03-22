43160e715f88ccfd0b4a1d243b3d0cc5bc3a2bb2
preshux <preshux@gmail.com>
2024-03-22 12:13:09 -0700

    complete_task

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Installs a package flask
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Installs a package werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
